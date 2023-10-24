66388cb6e1371711098f3ed8e85d3c6cde4053c9
WiratchawaKannika <kannikaw@kkumail.com>
2023-10-24 19:18:54 +0700

    uodate code train EM Classes

--- ViT_Classification/train_model.py
+++ ViT_Classification/train_model.py
@@ -32,8 +32,9 @@ def main():
     my_parser = argparse.ArgumentParser()
     my_parser.add_argument('--epochs', type=int, default=5000, help='number of epochs to train our network for')
     my_parser.add_argument('--gpu', type=int, default=1, help='Number GPU 0,1')
-    my_parser.add_argument('--numclass', type=int, default=1, help='[1, 3]')
-    my_parser.add_argument('--data_path', type=str, default='/home/kannika/code/Rheology2023/Rheology_Blood/Dataset_Rheology_Blood_Viscosity_HN_NBL-2dFFTdataset-3channels-6Fold-splitclass.csv')
+    #my_parser.add_argument('--numclass', type=int, default=1, help='[1, 3]')
+    my_parser.add_argument('--classname', type=str, help='[thalas2classes, thalas3classes, typeBEvsBM]')
+    my_parser.add_argument('--data_path', type=str, default='/home/kannika/code/Rheology2023/Rheology_Blood/Dataset_Blood_Viscosity-2dFFTdataset-3channels-3Fold-EMClasses.csv')
     my_parser.add_argument('--save_dir', type=str, help='Main Output Path', default="/media/tohn/HDD/rheology2023/ViTModel/Classification/Blood_Viscosity")
     my_parser.add_argument('--name', type=str, help='Name to save output in save_dir')
     my_parser.add_argument('--R', type=int, help='[1:R1, 2:R2]')
@@ -68,7 +69,6 @@ def main():
     os.makedirs(root_base, exist_ok=True)
     data_path = args.data_path
     BATCH_SIZE = args.batchsize
-    numclass = args.numclass
     ## train seting
     num_epochs = args.epochs
     IMAGE_SIZE = args.size
@@ -84,13 +84,21 @@ def main():
     print(f"[INFO]: For Train Set : With Shape {DFtrain.shape}")
     print(f"[INFO]: For Validation Set : With Shape {DFvalid.shape}")
     ### Get data Loader
-    if numclass == 3:
+    if args.classname == "thalas3classes":
+        numclass = 3
         colums_y = "subclass"
         class_mode_img = 'categorical'
         loss_smooth = tf.keras.losses.CategoricalCrossentropy(label_smoothing = 0.2)
         metrics_acc = 'accuracy'
-    elif numclass == 1:
+    elif args.classname == "thalas2classes":
+        numclass = 1
         colums_y = "classes_binary"
+        class_mode_img = 'raw'
+        loss_smooth = tf.keras.losses.BinaryCrossentropy(label_smoothing = 0.2)
+        metrics_acc = 'binary_accuracy'
+    elif args.classname == "typeBEvsBM":
+        numclass = 1
+        colums_y = "typeBEvsBM_binary"
         class_mode_img = 'raw'
         loss_smooth = tf.keras.losses.BinaryCrossentropy(label_smoothing = 0.2)
         metrics_acc = 'binary_accuracy'
@@ -114,7 +122,7 @@ def main():
     ##*** mkdir Modelname 
     modelNamemkdir = f"{root_base}/{args.FmodelsName}"
     os.makedirs(modelNamemkdir, exist_ok=True)
-    modelName = f"ViTb32_{args.numclass}Class_{_fold}_{_R}.h5"
+    modelName = f"ViTb32_{args.classname}Class_{_fold}_{_R}.h5"
     Model2save = f'{modelNamemkdir}/{modelName}'
     ##*** on_epoch_end
     root_Metrics = f'{root_base}/{args.epochendName}/'
